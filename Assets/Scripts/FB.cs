40781380f1e57aaa9e72571c8bfed0fc04bc0f17
Roblox Hero <th1624870@gmail.com>
2025-11-23 17:56:48 -0600

    Rename Firebase initializer file to FB.cs

---
+++ Assets/Scripts/FB.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading.Tasks;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine;
+
+namespace FB
+{
+    public static class FirebaseApp
+    {
+        static TaskCompletionSource<bool> _initializationTcs;
+        static FirebaseAuth _authInstance;
+
+        public static bool IsInitialized => _initializationTcs?.Task.IsCompletedSuccessfully ?? false;
+
+        public static Task<bool> InitializeAsync()
+        {
+            if (_initializationTcs != null)
+            {
+                return _initializationTcs.Task;
+            }
+
+            _initializationTcs = new TaskCompletionSource<bool>();
+            InitializeInternal();
+            return _initializationTcs.Task;
+        }
+
+        static async void InitializeInternal()
+        {
+            try
+            {
+                var dependencyStatus = await global::Firebase.FirebaseApp.CheckAndFixDependenciesAsync();
+                if (dependencyStatus == DependencyStatus.Available)
+                {
+                    _ = global::Firebase.FirebaseApp.DefaultInstance;
+                    _initializationTcs.TrySetResult(true);
+                    Debug.Log("[Firebase] Initialized default app.");
+                }
+                else
+                {
+                    Debug.LogError($"[Firebase] Dependencies unavailable: {dependencyStatus}");
+                    _initializationTcs.TrySetResult(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                _initializationTcs.TrySetException(ex);
+            }
+        }
+
+        public static async Task<FirebaseAuth> GetAuthAsync()
+        {
+            bool initialized = await InitializeAsync();
+            if (!initialized)
+            {
+                return null;
+            }
+
+            return _authInstance ??= FirebaseAuth.DefaultInstance;
+        }
+    }
+}
