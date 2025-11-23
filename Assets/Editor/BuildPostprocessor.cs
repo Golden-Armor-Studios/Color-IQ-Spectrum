0bb8c7b2f50f1d395b1d7acdab3f5f7bfa5d3d1c
Roblox Hero <th1624870@gmail.com>
2025-11-23 12:48:45 -0600

    Also disable entitlement write validation

--- Assets/Editor/BuildPostprocessor.cs
+++ Assets/Editor/BuildPostprocessor.cs
@@ -27,20 +27,25 @@ public static class BuildPostprocessor
 
         const string key = "CODE_SIGN_ALLOW_ENTITLEMENTS_MODIFICATION";
         const string value = "YES";
+        const string disableEntitlementsValidationKey = "CODE_SIGN_ALLOW_ENTITLEMENT_WRITES";
+        const string disableEntitlementsValidationValue = "YES";
 
         if (!string.IsNullOrEmpty(mainTarget))
         {
             project.SetBuildProperty(mainTarget, key, value);
+            project.SetBuildProperty(mainTarget, disableEntitlementsValidationKey, disableEntitlementsValidationValue);
         }
         if (!string.IsNullOrEmpty(frameworkTarget))
         {
             project.SetBuildProperty(frameworkTarget, key, value);
+            project.SetBuildProperty(frameworkTarget, disableEntitlementsValidationKey, disableEntitlementsValidationValue);
         }
 
         string projectGuid = project.ProjectGuid();
         if (!string.IsNullOrEmpty(projectGuid))
         {
             project.SetBuildProperty(projectGuid, key, value);
+            project.SetBuildProperty(projectGuid, disableEntitlementsValidationKey, disableEntitlementsValidationValue);
         }
 
         project.WriteToFile(projectPath);
